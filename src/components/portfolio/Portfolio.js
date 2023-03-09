3948d60b8545a50830d791acf01e994edf69eec8
noah berlove <noah.berlove@gmail.com>
2023-03-08 17:32:02 -0800

    project updates

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -7,13 +7,12 @@ import "./Portfolio.css"
 
 export default function Portfolio() {
     const [collection, setCollection] = React.useState([...info.portfolio])
-    console.log(collection)
     return (
         <Box>
-            <DropDown 
-                setCollection={setCollection}
-                collection={collection}
-                />
+            {/* <DropDown  */}
+                {/* // setCollection={setCollection} */}
+                {/* // collection={collection} */}
+                {/* // /> */}
             <Grid container display={'flex'} justifyContent={'center'}>
                 {collection.map((project, index) => (
                    <Grid item xs={12} md={6} key={index}>
